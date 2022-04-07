988e0a6fae4790776801cc76d5f63b927456f280
hongshibao <771989536@qq.com>
2022-04-07 21:29:02 +0800

    update

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,8 +1,7 @@
-
 /**
  * 创建store的方法，返回一个store（JS对象）
  * @param {*} reducer 处理器函数，根据旧的state和action来计算下一个最新的state
- * @returns 
+ * @returns
  */
 function createStore(reducer) {
   let state
@@ -12,31 +11,34 @@ function createStore(reducer) {
   }
 
   /**
-	 * 修改state的唯一方法：调用dispatch
-	 * @param {*} action 修改state的动作，是一个js对象，必须有type属性 { type: '@@REDUX/INIT' }
-	 */
+   * 修改state的唯一方法：调用dispatch
+   * @param {*} action 修改state的动作，是一个js对象，必须有type属性 { type: '@@REDUX/INIT' }
+   */
   function dispatch(action) {
     state = reducer(state, action)
 
-		// state更新后，执行所有订阅的回调
-		listeners.forEach(listener => listener())
+    // state更新后，执行所有订阅的回调
+    listeners.forEach((listener) => listener())
+
+    return action
   }
 
-	/**
-	 * 订阅
-	 * @param {*} listener 回调，在dispatch修改state后，会调用传进来的回调函数
-	 * @returns 返回取消订阅的方法，调用后就把当前的订阅删除
-	 */
+  /**
+   * 订阅
+   * @param {*} listener 回调，在dispatch修改state后，会调用传进来的回调函数
+   * @returns 返回取消订阅的方法，调用后就把当前的订阅删除
+   */
   function subscribe(listener) {
     listeners.push(listener)
-		return () => { // 删除对应的订阅
-			const index = listeners.indexOf(listener)
-			listeners.splice(index, 1)
-			// listeners = listeners.filter(l => l !== listener)
-		}
+    return () => {
+      // 删除对应的订阅
+      const index = listeners.indexOf(listener)
+      listeners.splice(index, 1)
+      // listeners = listeners.filter(l => l !== listener)
+    }
   }
 
-	dispatch({ type: '@@REDUX/INIT' }) // 源码里也是这样，初始化的时候会调用一下dispatch
+  dispatch({ type: '@@REDUX/INIT' }) // 源码里也是这样，初始化的时候会调用一下dispatch
 
   return {
     getState,
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,20 @@ import number2Reducer from './reducers/number2'
 
 const redecers = {
   num1: numberReducer,
-  num2: number2Reducer
+  num2: number2Reducer,
 }
 
 const combineReducer = combineReducers(redecers)
-const store = createStore(combineReducer) // { numberReducer: {num: 0}, number2Reducer: {num: 0} }  
+const store = createStore(combineReducer) // { numberReducer: {num: 0}, number2Reducer: {num: 0} }
+
+// redux 中间件的核心原理就是重写了dispatch方法，在原始的dispatch方法之前，或是之后加入了一些自定义的逻辑
+// 如下：实现一个简版的redux-logger
+// let dispatch = store.dispatch
+// store.dispatch = function (action) {
+//   console.log('prev state', store.getState())
+//   dispatch(action)
+//   console.log('next state', store.getState())
+//   return action
+// }
+
 export default store
